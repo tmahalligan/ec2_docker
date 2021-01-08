b41f19060ee2aae2cd5928eed06ff90ebd293c7e
Thomas Halligan <tmahalligan@gmail.com>
2021-01-07 20:27:04 -0500

    Security group mod

--- security-groups.tf
+++ security-groups.tf
@@ -10,6 +10,20 @@ resource "aws_security_group" "docker_host" {
     cidr_blocks = [format("%s/%s",data.external.whatismyip.result["internet_ip"],32)]
   }
 
+    ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
